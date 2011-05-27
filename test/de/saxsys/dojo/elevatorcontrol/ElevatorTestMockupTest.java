42846bbd4b1dc0740b23a64bdfa01836c0471fa0
Marco Dierenfeldt <marco.dierenfeldt@saxsys.de>
2011-05-27 07:37:00 +0200

    moved ElevatorTestMockup into impl package.

--- test/de/saxsys/dojo/elevatorcontrol/ElevatorTestMockupTest.java
+++ test/de/saxsys/dojo/elevatorcontrol/ElevatorTestMockupTest.java
@@ -4,6 +4,7 @@
  */
 package de.saxsys.dojo.elevatorcontrol;
 
+import de.saxsys.dojo.elevatorcontrol.impl.ElevatorTestMockup;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
--- test/de/saxsys/dojo/elevatorcontrol/impl/ElevatorControllerImplTest.java
+++ test/de/saxsys/dojo/elevatorcontrol/impl/ElevatorControllerImplTest.java
@@ -8,7 +8,6 @@ package de.saxsys.dojo.elevatorcontrol.impl;
 import de.saxsys.dojo.elevatorcontrol.Elevator;
 import de.saxsys.dojo.elevatorcontrol.Direction;
 import de.saxsys.dojo.elevatorcontrol.ElevatorLoggingObserver;
-import de.saxsys.dojo.elevatorcontrol.ElevatorTestMockup;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -52,26 +51,67 @@ public class ElevatorControllerImplTest {
      * Test of addCall method, of class ElevatorControllerImpl.
      */
     @Test
-    public void testAddCall() {
+    public void testAddCall() throws InterruptedException {
         System.out.println("addCall");
-        int level = 0;
-        ElevatorControllerImpl instance = new ElevatorControllerImpl(nullElevator);
+        int level = 6;
+        /*
+         * first 0 because of status change of direction NONE -> UP
+         * last five because of status change of direction UP -> NONE
+         */
+        int[] expectedLevels = {0, 0, 1, 2, 3, 4, 5, 6, 6};
+        Direction[] expectedDirections = {
+            Direction.UP,
+            Direction.UP,
+            Direction.UP,
+            Direction.UP,
+            Direction.UP,
+            Direction.UP,
+            Direction.UP,
+            Direction.UP,
+            Direction.NONE};            
+
+        ElevatorControllerImpl instance = new ElevatorControllerImpl(elevatorMock);
         instance.addCall(level);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        Thread.sleep(1000);
+        int[] actualLevels = observer.getLevelsLogArray();
+        assertArrayEquals(expectedLevels, actualLevels);
+
+        Direction[] actualDirections = observer.getDirectionsLogArray();
+        assertArrayEquals(expectedDirections, actualDirections);
+
     }
 
     /**
      * Test of addAssignment method, of class ElevatorControllerImpl.
      */
     @Test
-    public void testAddAssignment() {
+    public void testAddAssignment() throws InterruptedException {
         System.out.println("addAssignment");
-        int level = 0;
-        ElevatorControllerImpl instance = new ElevatorControllerImpl(nullElevator);
+        int level = 4;
+        /*
+         * first 0 because of status change of direction NONE -> UP
+         * last five because of status change of direction UP -> NONE
+         */
+        int[] expectedLevels = {0, 0, 1, 2, 3, 4, 4};
+        Direction[] expectedDirections = {
+            Direction.UP,
+            Direction.UP,
+            Direction.UP,
+            Direction.UP,
+            Direction.UP,
+            Direction.UP,
+            Direction.NONE};
+
+        ElevatorControllerImpl instance = new ElevatorControllerImpl(elevatorMock);
         instance.addAssignment(level);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        
+        Thread.sleep(1000);
+        
+        int[] actualLevels = observer.getLevelsLogArray();
+        assertArrayEquals(expectedLevels, actualLevels);
+
+        Direction[] actualDirections = observer.getDirectionsLogArray();
+        assertArrayEquals(expectedDirections, actualDirections);
     }
 
     /**
@@ -80,7 +120,7 @@ public class ElevatorControllerImplTest {
     @Test
     public void testGetElevatorPositionAtStart() {
         System.out.println("getElevatorPosition");
-        ElevatorControllerImpl instance = new ElevatorControllerImpl(nullElevator);
+        ElevatorControllerImpl instance = new ElevatorControllerImpl(elevatorMock);
         int expResult = 0;
         int result = instance.getElevatorPosition();
         assertEquals(expResult, result);
@@ -92,7 +132,7 @@ public class ElevatorControllerImplTest {
     @Test
     public void testGetElevatorDirectionAtStart() {
         System.out.println("getElevatorDirection");
-        ElevatorControllerImpl instance = new ElevatorControllerImpl(nullElevator);
+        ElevatorControllerImpl instance = new ElevatorControllerImpl(elevatorMock);
         Direction expResult = Direction.NONE;
         Direction result = instance.getElevatorDirection();
         assertEquals(expResult, result);
@@ -102,7 +142,7 @@ public class ElevatorControllerImplTest {
      * Test of doEmergencyShutdown method, of class ElevatorControllerImpl.
      */
     @Test
-    public void testDoEmergencyShutdown() {
+    public void testDoEmergencyShutdown() throws InterruptedException {
         System.out.println("doEmergencyShutdown");
         int level = 5;
         /*
@@ -130,6 +170,7 @@ public class ElevatorControllerImplTest {
 
         ElevatorControllerImpl instance = new ElevatorControllerImpl(elevatorMock);
         instance.addAssignment(5);
+        Thread.sleep(1000);
         instance.doEmergencyShutdown();
 
         int[] actualLevels = observer.getLevelsLogArray();
